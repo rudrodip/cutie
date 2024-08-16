cea0c978e087fc02778cdf16139562b91b93b241
rudrodip <official.rudrodipsarker@gmail.com>
2024-08-16 14:13:49 +0600

    updated

---
+++ src/config/site.config.ts
@@ -0,0 +1,17 @@
+export const siteConfig = {
+  name: "What do you want? 👀",
+  description: "tell me what do you want? 👀",
+  url: "https://cutie.rdsx.dev",
+  keywords: [
+    "meme",
+    "doraemon meme",
+    "anime meme",
+    "vercel og",
+    "image generation"
+  ],
+  ogImage: "https://cutie.rdsx.dev/og.png",
+  links: {
+    twitter: "https://x.com/rds_agi",
+    github: "https://github.com/rudrodip/cutie"
+  }
+};
