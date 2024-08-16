a220399aa0604c0224a63d39c15083a3ac849a99
rudrodip <official.rudrodipsarker@gmail.com>
2024-08-16 14:13:04 +0600

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,70 @@
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { siteConfig } from "@/config/site.config";
+import { Suspense } from "react";
+import { Toaster } from "@/components/ui/sonner";
+
+const fontSans = Manrope({ subsets: ["latin"], variable: "--font-sans" });
+const fontHeading = Manrope({ subsets: ["latin"], variable: "--font-heading" });
+
+export const metadata: Metadata = {
+  metadataBase: new URL(siteConfig.url),
+  title: siteConfig.name,
+  description: siteConfig.description,
+  keywords: siteConfig.keywords,
+  creator: siteConfig.name,
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+  },
+  // OpenGraph metadata
+  openGraph: {
+    title: siteConfig.name,
+    description: siteConfig.description,
+    url: siteConfig.url,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1120,
+        height: 1240,
+        alt: siteConfig.name,
+      },
+    ],
+    type: "website",
+    locale: "en_US",
+  },
+
+  // Twitter metadata
+  twitter: {
+    card: "summary_large_image",
+    site: "@rds_agi",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: {
+      url: siteConfig.ogImage,
+      width: 1120,
+      height: 1240,
+      alt: siteConfig.name,
+    },
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn(fontHeading.className, fontSans.className, "w-full h-[100dvh] flex justify-center items-center")}>
+        <Suspense fallback={null}>
+          <Toaster position="top-right" />
+          {children}
+        </Suspense>
+      </body>
+    </html>
+  );
+}
